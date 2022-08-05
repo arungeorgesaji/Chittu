c4fdf76445064b86422930b6036be2ecaac75344
arungeorgesaji <109901706+arungeorgesaji@users.noreply.github.com>
2022-08-05 15:56:35 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,75 @@
+import json
+import pickle
+import random
+
+import numpy as np
+import nltk
+from nltk.stem import WordNetLemmatizer
+
+import tensorflow as tf
+keras = tf.keras
+k = keras.backend
+kl = keras.layers
+km = keras.models
+ko = keras.optimizers
+Sequential = km.Sequential
+Dense, Dropout, Activation = kl.Dense, kl.Dropout, kl.Activation
+SGD = ko.SGD
+
+lemmatizer = WordNetLemmatizer()
+
+intents = json.loads(open('intents.json').read())
+
+words = []
+classes = []
+documents = []
+ignore_letters = ['?', '!', '.', ',']
+
+for intent in intents['intents']:
+    for pattern in intent['patterns']:
+        word_list = nltk.word_tokenize(pattern)
+        words.extend(word_list)
+        documents.append((word_list, intent['tags']))
+        if intent['tags'] not in classes:
+            classes.append(intent['tags'])
+
+words = [lemmatizer.lemmatize(word) for word in words if word not in ignore_letters]
+
+words = sorted(set(words))
+
+classes = sorted(set(classes))
+
+pickle.dump(words, open('words.pkl', 'wb'))
+pickle.dump(classes, open('classes.pkl', 'wb'))
+
+training = []
+output_empty = [0] * len(classes)
+
+for document in documents:
+    bag = []
+    word_patterns = document[0]
+    word_patterns = [lemmatizer.lemmatize(word.lower()) for word in word_patterns]
+    for word in words:
+        bag.append(1) if word in word_patterns else bag.append(0)
+        output_row = list(output_empty)
+        output_row[classes.index(document[1])] = 1
+        training.append([bag, output_row])
+
+random.shuffle(training)
+training = np.array(training)
+
+train_x = list(training[:, 0])
+train_y = list(training[:, 1])
+
+model = keras.Sequential()
+model.add(Dense(128, input_shape=(len(train_x[0]),), activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(64, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(len(train_y[0]), activation='softmax'))
+
+sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+hist = model.fit(np.array(train_x), np.array(train_y), epochs=50, batch_size=32 , verbose=1)
+model.save('ai.h5', hist)
+print('done')
